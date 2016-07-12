972addf03bacbbf2416da9f18ed5d5166757fda5
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-07-11 20:51:29 -0400

    Add solution code

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,4 +1,10 @@
 
 def make_change(price, amnt_tendered, denoms = [25, 10, 5, 1])
   # Your Code Here
+  change_left_to_give = ((amnt_tendered - price.to_f) * 100).round.to_i
+  denoms.map do |denom|
+    num_coins = change_left_to_give / denom
+    change_left_to_give = change_left_to_give % denom
+    num_coins
+  end
 end
